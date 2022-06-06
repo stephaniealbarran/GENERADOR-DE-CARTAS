d1188b188a8e735bf2a93f01d155ab47e0d4085d
stephaniealbarran <stefanyalbarran@hotmail.com>
2022-06-06 18:44:42 +0000

    agrego ejercicio hecho

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  window.genCard = function() {
+    let iconos = ["♥", "♠", "♣", "♦"];
+    let numeros = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    function randomCard() {
+      const randomIconos = iconos[Math.floor(Math.random() * iconos.length)];
+      const randomNumeros = numeros[Math.floor(Math.random() * numeros.length)];
+
+      document.querySelector(".icon").innerHTML = randomIconos;
+      document.querySelector(".cardContentContainer").innerHTML = randomNumeros;
+      document.querySelector(".iconTwo").innerHTML = randomIconos;
+
+      if (randomIconos == "♥" || randomIconos == "♦") {
+        document.querySelector("#card").style.color = "red";
+      } else if (randomIconos == "♠" || randomIconos == "♣") {
+        document.querySelector("#card").style.color = "black";
+      }
+    }
+
+    randomCard();
+  };
+
+  window.genCard();
 };
